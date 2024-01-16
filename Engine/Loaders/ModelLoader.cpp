558f2a165c0657805b6703877e2520540175429c
Anthk <tkabanets@hotmail.com>
2024-01-15 19:01:16 -0800

    Improved speed and reduced code complexity of ObjLoader::loadObj()

--- Engine/Loaders/ModelLoader.cpp
+++ Engine/Loaders/ModelLoader.cpp
@@ -1,26 +1,11 @@
 #include <GL\glew.h>
 #include <fstream>
-#include <iostream>
 #include <vector>
 #include <string>
 #include "ModelLoader.h"
 #include <GLM/vec3.hpp>
 #include <GLM/vec2.hpp>
-
-std::vector<std::string> splitLine(std::string line, char delim) {
-	std::vector<std::string> vals;
-	std::string subStr;
-	int pos;
-	pos = line.find(delim);
-	while (pos != std::string::npos) {
-		vals.push_back(line.substr(0, pos));
-		line.erase(0, pos + 1);
-		pos = line.find(delim);
-	}
-	vals.push_back(line);
-
-	return vals;
-}
+#include <sstream>
 
 namespace FileLoader {
 	void ModelLoader::clearVals()
@@ -37,9 +22,10 @@ namespace FileLoader {
 		std::vector<glm::vec3> normals;
 		std::vector<float> triangles;
 		std::ifstream objFile(filePath);
-
+		
 		std::string buffer;
-		std::vector<std::string> temp;
+		int face[3][3];
+		char discard;
 		glm::vec3 point(0, 0, 0);
 		glm::vec2 tex(0, 0);
 
@@ -47,45 +33,46 @@ namespace FileLoader {
 			throw("ifstream: " + filePath + " failed to load.");
 
 		while (std::getline(objFile, buffer)) {
-			if (buffer[0] == '#')
+			std::stringstream iss(buffer);
+			std::string type;
+			iss >> type;
+			if (type == "#")
 				continue;
-			if (buffer.rfind("v ", 0) == 0) {
-				temp = splitLine(buffer, ' ');
-				point = glm::vec3(std::stof(temp[1]), std::stof(temp[2]), std::stof(temp[3]));
+			if (type == "v") {
+				iss >> point.x >> point.y >> point.z;
 				vertices.push_back(point);
 			}
-			else if (buffer.rfind("vt ", 0) == 0) {
-				temp = splitLine(buffer, ' ');
-				tex = glm::vec2(std::stof(temp[1]), std::stof(temp[2]));
+			else if (type == "vt") {
+				iss >> tex.x >> tex.y;
 				textures.push_back(tex);
 			}
-			else if (buffer.rfind("vn ", 0) == 0) {
-				temp = splitLine(buffer, ' ');
-				point = glm::vec3(std::stof(temp[1]), std::stof(temp[2]), std::stof(temp[3]));
+			else if (type == "vn") {
+				iss >> point.x >> point.y >> point.z;
 				normals.push_back(point);
 			}
-			else if (buffer.rfind("f ", 0) == 0) {
-				temp = splitLine(buffer, ' ');
-				//Should split into 4.
-				std::vector<std::string> triangles;
+			else if (type == "f") {
+				iss >> face[0][0] >> discard >> face[0][1] >> discard >> face[0][2]
+					>> face[1][0] >> discard >> face[1][1] >> discard >> face[1][2]
+					>> face[2][0] >> discard >> face[2][1] >> discard >> face[2][2];
+				
 				for (int i = 0; i < 3; ++i) {
-					triangles = splitLine(temp[i + 1], '/');
-					int vertexID = std::stoi(triangles[0]);
-					point = vertices[vertexID - 1];
+					point = vertices[face[i][0] - 1];
 					vertexList.push_back(point.x);
 					vertexList.push_back(point.y);
 					vertexList.push_back(point.z);
 
-					int texID = std::stoi(triangles[1]);
-					tex = textures[texID - 1];
-					texList.push_back(tex.x);
-					texList.push_back(tex.y);
+					if(genTextures) {
+						tex = textures[face[i][1] - 1];
+						texList.push_back(tex.x);
+						texList.push_back(tex.y);
+					}
 
-					int normID = std::stoi(triangles[2]);
-					point = normals[normID - 1];
-					normalList.push_back(point.x);
-					normalList.push_back(point.y);
-					normalList.push_back(point.z);
+					if (genNormals) {
+						point = normals[face[i][2] - 1];
+						normalList.push_back(point.x);
+						normalList.push_back(point.y);
+						normalList.push_back(point.z);
+					}
 				}
 			}
 		}
--- Engine/Loaders/ModelLoader.h
+++ Engine/Loaders/ModelLoader.h
@@ -9,6 +9,9 @@ namespace FileLoader {
 		std::vector<float> texList;
 		std::vector<float> normalList;
 	public:
+		bool genTextures = true;
+		bool genNormals = true;
+		//bool genTangents = false;
 		std::vector<float> getVertices() { return vertexList; }
 		std::vector<float> getTexCoords() {	return texList; }
 		std::vector<float> getNormals() { return normalList; }
