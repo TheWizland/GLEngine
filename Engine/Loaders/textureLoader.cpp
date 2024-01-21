d7b87eb8322e511f536902ee624d4ce58907603c
Anthk <tkabanets@hotmail.com>
2024-01-20 16:28:33 -0800

    Added mipmapping and anisotropic filtering.

--- Engine/Loaders/textureLoader.cpp
+++ Engine/Loaders/textureLoader.cpp
@@ -19,8 +19,16 @@ namespace FileLoader {
 		glBindTexture(GL_TEXTURE_2D, textureID);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		if(glewIsSupported("GL_EXT_texture_filter_anisotropic")) {
+			GLfloat anisoSetting = 0.0f;
+			glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &anisoSetting);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, anisoSetting);
+		}
 
 		return textureID;
 	}
